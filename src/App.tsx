090da2ddabbe17bf97d4730bac19ef474dd1df9f
Valeriy <masterbox06@gmail.com>
2023-08-15 19:20:30 +0400

    cp 1 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {SuperPuperButton} from "./SuperPuperButton";
 
 function App() {
+  const [count, setCount] = useState<number>(0)
+
+  const resetHandler = () => {
+    console.log("reset")
+    setCount(0)
+  }
+
+  const incHandler = () => {
+    console.log("inc")
+    if (count < 5) setCount(count + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen bg-gray-800 flex justify-center items-center text-gray-800">
+      <div className="border-8 border-cyan-500 rounded-md border-box p-8 flex flex-col gap-8">
+
+        <div className="bg-cyan-500 border-box font-bold flex justify-center rounded border-box p-6 cursor-default">
+          <h1 className={`${count === 5 ? "text-red-600" : ""} text-8xl`}>{count}</h1>
+        </div>
+
+        <div className="flex flex-row gap-8 border-8 border-cyan-500 rounded-md border-box p-8">
+          <SuperPuperButton title="inc" active={count < 5} callBack={incHandler}/>
+          <SuperPuperButton title="reset" active={count > 0} callBack={resetHandler}/>
+        </div>
+
+      </div>
     </div>
   );
 }
